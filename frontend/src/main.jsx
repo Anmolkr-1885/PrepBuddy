ba0e0847ef4c81770ae2f819d816e9371daf6f97
ANMOL KUMAR GUPTA <137256041+Anmolkr-1885@users.noreply.github.com>
2026-08-21 12:00:24 +0530

    database setup added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ClerkProvider } from '@clerk/react'
+
+const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!publishableKey) {
+  throw new Error('Missing Clerk Publishable Key')
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={publishableKey}>
+      <App />
+    </ClerkProvider>
   </StrictMode>,
 )
