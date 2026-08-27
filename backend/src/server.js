7e581070c9b7b2f1bb4df02a4063d5ce49d5b9a5
ANMOL KUMAR GUPTA <137256041+Anmolkr-1885@users.noreply.github.com>
2026-08-27 16:48:28 +0530

    server-side-protect-route

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,9 @@ import { connectDB } from "./lib/db.js";
 import { serve } from "inngest/express";
 import cors from "cors";
 import { inngest, functions } from "./lib/inngest.js";
+import { clerkMiddleware } from "@clerk/express";
+import chatRoutes from "./routes/chatRoutes.js";
+
 
 
 
@@ -17,9 +20,14 @@ app.use(express.json());
 // credentials:true meaning?? => server allows a browser to include cookies on request
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
+
+
 const __dirname = path.resolve();
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.use("/api/chat", chatRoutes);
 
 
 app.get("/health",(req,res)=>{
