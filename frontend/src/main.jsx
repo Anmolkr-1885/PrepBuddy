81a6d1f0368685a7f227130a92a4263d8ddc03db
ANMOL KUMAR GUPTA <137256041+Anmolkr-1885@users.noreply.github.com>
2026-08-28 07:45:46 +0530

    frontend setup completed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { ClerkProvider } from '@clerk/react'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+// Import Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-if (!publishableKey) {
-  throw new Error('Missing Clerk Publishable Key')
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Clerk Publishable Key");
 }
 
-createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ClerkProvider publishableKey={publishableKey}>
-      <App />
-    </ClerkProvider>
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+          <App />
+        </ClerkProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
